df43f62d2034eecfe410492fc3221f37a6ff202e
Parola Marco <mark.prl.97@gmail.com>
2020-01-03 16:25:19 +0100

    Primo tentativo di fixaggio

--- src/dos/SubscribeCallback.java
+++ src/dos/SubscribeCallback.java
@@ -6,23 +6,39 @@ import org.eclipse.paho.client.mqttv3.MqttMessage;
 
 public class SubscribeCallback implements MqttCallback {
 
+	private Object lock;
+
+
+	public SubscribeCallback(Object lock) {
+		if(lock == null) {
+			this.lock = new Object();
+		} else {
+			this.lock = lock;
+		}
+	}
+
+	public SubscribeCallback() {
+		this(null); }
+
+
 	@Override
 	public void connectionLost(Throwable arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void deliveryComplete(IMqttDeliveryToken arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void messageArrived(String topic, MqttMessage mqttMessage) throws Exception {
-		System.out.println("Message arrived from topic: " + topic + " Message content: " + mqttMessage.toString());
-		
-		if(topic.equals("home/LWT")) System.out.println("Sensor gone");
+		synchronized(lock) {
+			System.out.println("Message arrived from topic: " + topic + " Message content: " + mqttMessage.toString());
+			if(topic.equals("home/LWT")) System.out.println("Sensor gone");
+		}
 	}
 
 }
--- src/dos/Subscriber.java
+++ src/dos/Subscriber.java
@@ -8,64 +8,76 @@ import org.eclipse.paho.client.mqttv3.MqttException;
 
 public class Subscriber extends Thread {
 	private static int NUM_THREADS = 300;
-	
-	private static int DELAY_THREADS = 50;
-	
+
 	// Url del brocker
-	public static final String BROKER_URL = "tcp://localhost:1883";
-	
+	public static final String BROKER_URL = "tcp://mqtt.eclipse.org:1883";
+
 	// Id del client generato dinamicamente ottenendo il numero di secondi
 	public String clientId = null;
-	
+
 	// Il topic a cui sottoscrivere
 	private static final String TOPIC = "dos/dosMult";
-	
+
 	// client mqtt
 	private MqttClient mqttClient;
 
 	public int counter = 0;
-	
-	public Subscriber(int i) {
+
+	private Object lock;
+
+
+	public Subscriber(Object lock, int i) {
+		if(lock == null) {
+			this.lock = new Object();
+		} else {
+			this.lock = lock;
+		}
+
 		try {
-			counter = i;
-			clientId = Long.toString(new Date().getTime()) + "-sub";
-			// crea un nuovo MqttCLient con l'url del brocker e l'id del client
-			mqttClient = new MqttClient(BROKER_URL, clientId);
+			synchronized(this.lock) {
+				counter = i;
+				clientId = Long.toString(new Date().getTime()) + "-sub";
+				// crea un nuovo MqttCLient con l'url del brocker e l'id del client
+				mqttClient = new MqttClient(BROKER_URL, clientId);
+			}
 		} catch(MqttException e) {
 			e.printStackTrace();
 			System.exit(1);
-		} 
+		}
 	}
-	
+
+	public Subscriber(int i) {
+		this(null, i); }
+
+
+	@Override
 	public void run() {
 		try {
 			// Imposta le callback
-			mqttClient.setCallback(new SubscribeCallback());
+			mqttClient.setCallback(new SubscribeCallback(lock));
 			// Si connette al broker
 			mqttClient.connect();
-			
+
 			// Sottoscrive al topic desiderato
 			mqttClient.subscribe(TOPIC);
-			
+
 			System.out.println("Now subscriber " + counter + " listening on topic: " + TOPIC);
 		} catch (MqttException e) {
 			e.printStackTrace();
-			System.exit(1);			
+			System.exit(1);
 		}
 	}
-	
+
 	public static void main(String args[]) throws InterruptedException {
 		ArrayList<Subscriber> listSub = new ArrayList<Subscriber>();
 		for(int i = 1; i <= NUM_THREADS; i++) {
-			listSub.add(new Subscriber(i));
+			listSub.add(new Subscriber(listSub, i));
 			listSub.get(listSub.size()-1).run();
-			
-			Thread.sleep(DELAY_THREADS);
 		}
-		
+
 		System.out.println("Fine");
 	}
-	
+
 	// Provare uno con tanti e tanti con tanti
-	
+
 }
